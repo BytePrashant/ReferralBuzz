86b91e930b7b88a269d5590c25ab0034ba68dd6d
Prashant Tomar <prashanttomar765@gmail.com>
2024-05-24 02:35:23 +0530

    made few changes

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -1,25 +1,31 @@
 import React from "react";
-import { cn } from "@/utils/cn";
 import { Spotlight } from "./ui/Spotlight";
 
 export function Homepage() {
   return (
     <div className="h-[45rem] w-full flex items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
-      <Spotlight
-        className="-top-40 left-0 md:left-60 md:-top-20"
-        fill="pink"
-      />
+      <Spotlight className="-top-40 left-0 md:left-60 md:-top-20" fill="pink" />
       <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
         <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
           Spotlight <br /> is the new trend.
         </h1>
+        <Spotlight
+          className="-top-40 left-0 md:left-60 md:-top-40"
+          fill="Purple"
+        />
         <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
           Spotlight effect is a great way to draw attention to a specific part
           of the page. Here, we are drawing the attention towards the text
           section of the page. I don&apos;t know why but I&apos;m running out of
           copy.
         </p>
-        <div className="flex justify-center items-center mt-4">
+        <div className="flex justify-center items-center mt-8">
+          <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+            <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+              Border Magic
+            </span>
+          </button>
         </div>
       </div>
     </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,17 +3,6 @@ import React, { useState } from "react";
 import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
 import { cn } from "@/utils/cn";
 
-export function NavbarDemo() {
-  return (
-    <div className="relative w-full flex items-center justify-center">
-      <Navbar className="top-2" />
-      <p className="text-black dark:text-white">
-        The Navbar will show on top of the page
-      </p>
-    </div>
-  );
-}
-
 export function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
   return (
