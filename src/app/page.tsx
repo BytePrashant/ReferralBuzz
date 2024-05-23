fc3723f456cd2546cd8bf9882af30f09aaf9e62b
Prashant Tomar <prashanttomar765@gmail.com>
2024-05-24 02:36:26 +0530

    added FeaturedCourses component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { FeaturedCourses } from "@/components/FeaturedCourses";
 import { Homepage } from "@/components/Homepage";
 
 export default function Home() {
   return (
     <div>
       <Homepage />
+      <FeaturedCourses />
     </div>
   );
 }
